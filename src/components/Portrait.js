753cc8fd498894610d304740270cf4e9b21f0c12
Marianna Cherniuk <Marianna@Mariannas-MacBook-Air.local>
2019-12-17 14:51:24 -0800

    almost done

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import ImageP1 from '../assets/Portrait-Compress/DSC_0039.jpg'
+import ImageP2 from '../assets/Portrait-Compress/DSC_0047.jpg'
+import ImageP3 from '../assets/Portrait-Compress/DSC_0054.jpg'
+import ImageP4 from '../assets/Portrait-Compress/DSC_0122.jpg'
+import ImageP5 from '../assets/Portrait-Compress/DSC_0141.jpg'
+import ImageP6 from '../assets/Portrait-Compress/DSC_0173-2.jpg'
+import ImageP7 from '../assets/Portrait-Compress/DSC_0213.jpg'
+import ImageP8 from '../assets/Portrait-Compress/DSC_0154.jpg'
+import ImageP9 from '../assets/Portrait-Compress/DSC_0156.jpg'
+import ImageP10 from '../assets/Portrait-Compress/DSC_0231.jpg'
+import ImageP11 from '../assets/Portrait-Compress/DSC_0382.jpg'
 
 function Portrait() {
     return (
         <div className='portrait-container'>
-            <p>Hello from portrait</p>
+            <div className='portrait-container__grid'>
+                <div className='portrait-container__grid__galeryOneP'>
+                    <img className='image' src={ImageP1} alt='picture P1'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP'>
+                    <img className='image' src={ImageP2} alt='picture P2'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP'>
+                    <img className='image' src={ImageP3} alt='picture P3'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP'>
+                    <img className='image' src={ImageP4} alt='picture P4'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP'>
+                    <img className='image' src={ImageP5} alt='picture P5'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP'>
+                    <img className='image' src={ImageP6} alt='picture P6'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP__galerySevenP'>
+                    <img className='image' src={ImageP7} alt='picture P7'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP__galerySevenP__galeryEightP'>
+                    <img className='image' src={ImageP8} alt='picture P8'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP__galerySevenP__galeryEightP__galeryNineP'>
+                    <img className='image' src={ImageP9} alt='picture P9'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP__galerySevenP__galeryEightP__galeryNineP__galeryTenP'>
+                    <img className='image' src={ImageP10} alt='picture P10'/>
+                </div>
+                <div className='portrait-container__grid__galeryOneP__galeryTwoP__galeryThreeP__galeryFourP__galeryFiveP__galerySixP__galerySevenP__galeryEightP__galeryNineP__galeryTenP__galeryElevenP'>
+                    <img className='image' src={ImageP11} alt='picture P11'/>
+                </div>
+            </div>
         </div>
     )
 }
