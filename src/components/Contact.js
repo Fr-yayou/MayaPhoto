a247e20d6f0d684606811af08f36af95cba7e87e
yanis bekhtaoui <yanis.bekhtaoui@gmail.com>
2020-02-25 12:22:39 -0800

    last

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,27 +8,27 @@ function Contact() {
 </p>
             </div>
             <div className='container-form'>
-                <form className='container-form__submit' name='contact'data-netlify="true" action="POST" >
+                <form className='container-form__submit' name='contact'data-netlify="true" action="POST" data-netlify-honeypot="bot-field" >
                     <div id='containerName'>
                         <label>First Name *</label>
-                        <input id="name" type='text' name='firstName' />
+                        <input id="name" type='text' name='firstName' type="hidden" />
                         <label>Last Name *</label>
-                        <input  id='lastName'trype='text' name='familyName' />
+                        <input  id='lastName'trype='text' name='familyName' type="hidden" />
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>Email Address *</label>
-                        <input id='email' trype='text' name='email' />
+                        <input id='email' trype='text' name='email' type="hidden" />
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>Your Story *</label>
-                        <input id='yourStory' trype='text' name='description' />
+                        <input id='yourStory' trype='text' name='description' type="hidden" />
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>How did you hear about me ?</label>
-                        <input id='HowDidYou' trype='text' name='networking' />
+                        <input id='HowDidYou' trype='text' name='networking' type="hidden" />
                     </div>
                     <div className='container-form__submit__containerLabel__btn'>
-                        <input id='submitBtn' type='submit' name='btn'/>
+                        <input id='submitBtn' type='submit' name='btn' type="hidden"/>
                     </div>
                 </form>
             </div>
