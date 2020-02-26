48a3f2731e70d8d5819f4153c16f0a53030e4515
yanis bekhtaoui <yanis.bekhtaoui@gmail.com>
2020-02-25 23:59:22 -0800

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,34 @@
 import React from 'react'
 
 function Contact() {
+
+    const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+    state = {
+        firstName: '',
+        lastName:'',
+        email: '',
+        description: '',
+        networking:'',
+    }
+
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
     return (
         <div className='contact-container'>
             <div className='container-description-text'>
@@ -8,24 +36,24 @@ function Contact() {
 </p>
             </div>
             <div className='container-form'>
-                <form className='container-form__submit' data-netlify="true" action="POST"  netlify netlify-honeypot="bot-field" name='contact' >
+                <form className='container-form__submit' name='contact' onSubmit={this.handleSubmit} >
                     <div id='containerName'>
                         <label>First Name *</label>
-                        <input id="name" type='text' name='firstName' />
+                        <input id="name" type='text' name='firstName' value={firstName} onChange={this.handleChange}  />
                         <label>Last Name *</label>
-                        <input  id='lastName'trype='text' name='familyName' />
+                        <input  id='lastName'trype='text' name='lastName' value={lastName} onChange={this.handleChange}  />
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>Email Address *</label>
-                        <input id='email' trype='text' name='email' />
+                        <input id='email' trype='text' name='email' value={email} onChange={this.handleChange}/>
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>Your Story *</label>
-                        <input id='yourStory' trype='text' name='description' />
+                        <input id='yourStory' trype='text' name='description' value={description} onChange={this.handleChange} />
                     </div>
                     <div className='container-form__submit__containerLabel'>
                         <label>How did you hear about me ?</label>
-                        <input id='HowDidYou' trype='text' name='networking' />
+                        <input id='HowDidYou' trype='text' name='networking' value={networking}  onChange={this.handleChange}   />
                     </div>
                     <div className='container-form__submit__containerLabel__btn'>
                         <input id='submitBtn' type='submit' name='btn'/>
